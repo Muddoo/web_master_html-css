b78ca9c47e4b726e6c7b6f7daa8fa42389b596dc
Moddoo <>
2021-02-10 14:27:23 -0600

    hero

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible_expanded");
+  item.addEventListener("click", function (e) {
+     if(!e.target.matches('img') && !e.target.matches('a')) this.classList.toggle("collapsible_expanded");
   })
 );
